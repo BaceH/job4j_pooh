035e6651509dffef2fdf39fed92c09f2e075a7b1
VasylB <vasiliy.byalyk@dstarlab.com>
2021-03-05 14:56:54 +0200

    new version

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -1,38 +1,9 @@
 package ru.job4j;
 
-import ru.job4j.client.PoohClientSocket;
-import ru.job4j.server.PoohServerSocket;
+import ru.job4j.pooh.PoohServer;
 
 public class Main {
-    public static void main( String[] args )
-    {
-        String server = "localhost";
-        int port = 8081;
-        if( args[0].equals("server") )
-        {
-            System.out.println( "Start server...");
-
-            PoohServerSocket poohServerSocket = new PoohServerSocket(port);
-            poohServerSocket.startServer();
-
-            try {
-                Thread.sleep( 60000 );
-                poohServerSocket.stopServer();
-            } catch( Exception e ) {
-                e.printStackTrace();
-            }
-
-        }else if( args[0].equals("client") ){
-
-            System.out.println("Start client...");
-            PoohClientSocket poohClientSocket = new PoohClientSocket(server, port);
-            poohClientSocket.startClient();
-
-        } else {
-            System.out.println("Wrong argument");
-            System.exit(0);
-        }
-
-
+    public static void main( String[] args ){
+        new PoohServer().start();
     }
 }
