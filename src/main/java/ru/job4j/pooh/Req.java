346a217ed6709a6fbba48e59eff70298700ed3b1
VasylB <vasiliy.byalyk@dstarlab.com>
2021-03-10 17:19:11 +0200

    1. Техническое задание - проект Pooh JMS [#454106] first commit

--- src/main/java/ru/job4j/pooh/Req.java
+++ src/main/java/ru/job4j/pooh/Req.java
@@ -9,6 +9,7 @@ public class Req {
     private final String reqMethod;
     private final String nameQueue;
     private final String message;
+    private final int idConsumer;
 
     private final String param;
 
@@ -16,13 +17,21 @@ public class Req {
     public Req(String text) {
 
         List<String> reqLines = splitReq(text, "\n");
-
         this.param = reqLines.get(0);
-        this.reqMode = splitReq(splitReq(param, " ").get(1), "/").get(1);
+
+        List<String> paramList = splitReq(splitReq(param, " ").get(1), "/");
+
+        this.reqMode = paramList.get(1);
         this.reqMethod = splitReq(param, " ").get(0);
-        this.nameQueue = splitReq(splitReq(param, " ").get(1), "/").get(2);
+        this.nameQueue = paramList.get(2);
         this.message =  reqLines.get(reqLines.size() - 1);
         this.text = text;
+
+        if (paramList.size() >= 4) {
+            idConsumer = Integer.parseInt(paramList.get(3));
+        } else {
+            idConsumer = -1;
+        }
     }
 
     public String valueOf(String key) {
@@ -52,7 +61,9 @@ public class Req {
     public String getMessage() {
         return message;
     }
-
+    public int getIdConsumer() {
+        return idConsumer;
+    }
     private List<String> splitReq(String str, String regex){
         return Arrays.asList(str.split(regex));
     }
--- src/main/java/ru/job4j/pooh/TopicService.java
+++ src/main/java/ru/job4j/pooh/TopicService.java
@@ -1,8 +1,72 @@
 package ru.job4j.pooh;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class TopicService implements Service {
+    private final Map<String, ConcurrentLinkedQueue<String>> topic = new ConcurrentHashMap<>();
+    private final Map<Integer,Map<String, ConcurrentLinkedQueue<String>>> consumerTopic = new ConcurrentHashMap<>();
+
+    private boolean put(String name, String text) {
+
+        if (!topic.containsKey(name)) {
+            topic.put(name, new ConcurrentLinkedQueue<>());
+            if (!consumerTopic.isEmpty()){
+                for (Map.Entry<Integer,Map<String, ConcurrentLinkedQueue<String>>> entry : consumerTopic.entrySet()) {
+                    entry.getValue().put(name, new ConcurrentLinkedQueue<>());
+                }
+            }
+        }
+        if (!consumerTopic.isEmpty()){
+            for (Map.Entry<Integer,Map<String, ConcurrentLinkedQueue<String>>> entry : consumerTopic.entrySet()) {
+                entry.getValue().get(name).offer(text);
+            }
+        }
+        return topic.get(name).offer(text);
+    }
+
+    private String take(Req req) {
+        if (!consumerTopic.containsKey(req.getIdConsumer())){
+            Map<String, ConcurrentLinkedQueue<String>> topicId = new ConcurrentHashMap<>();
+
+            for (Map.Entry<String, ConcurrentLinkedQueue<String>> entry : topic.entrySet()) {
+                topicId.put(entry.getKey(),  new ConcurrentLinkedQueue<>(entry.getValue()));
+
+            }
+
+            consumerTopic.put(req.getIdConsumer(), topicId);
+        }
+
+        return consumerTopic.get(req.getIdConsumer()).get(req.getNameQueue()).poll();
+    }
+
     @Override
     public Resp process(Req req) {
-        return null;
+        System.out.println("TopicService process");
+        int status = 200;
+        String text;
+        switch (req.method()) {
+            case "GET":
+                text = take(req);
+                if (text == null) {
+                    status = 404;
+                    text = "\n\n queue is empty";
+                } else {
+                    text = "\n\n" + text;
+                }
+                break;
+            case "POST":
+                if (!put(req.getNameQueue(), req.getMessage()))
+                    status = 404;
+                text = req.getText();
+                break;
+            default:
+                status = 404;
+                text = "null";
+
+        }
+
+        return new Resp(text, status);
     }
 }
